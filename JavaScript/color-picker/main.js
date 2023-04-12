ac478596fb8d7d2d03d40b52aeddce9e069a6482
Yuli Kim <kkimyulee@naver.com>
2023-04-12 13:53:13 +0900

    [제거] Color Picker

--- JavaScript/color-picker/main.js
+++
@@ -1,9 +0,0 @@
-const inpPicker = document.querySelectorAll(".inp-picker");
-const root = document.querySelector(":root");
-
-inpPicker.forEach((item) => {
-  item.addEventListener("input", () => {
-    const name = `--main-${item.dataset.id}`;
-    root.style.setProperty(name, item.value);
-  });
-});
