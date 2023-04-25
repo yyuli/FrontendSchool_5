6b1216124f383d54935889904f6f46c79376ccac
Yuli Kim <kkimyulee@naver.com>
2023-04-25 10:04:50 +0900

    [실습] 수정 기능 추가

--- JavaScript/notepad/main.js
+++ JavaScript/notepad/main.js
@@ -2,38 +2,10 @@
 const title = document.querySelector("#memo-title");
 const contents = document.querySelector(".memo-contents");
 const btnComplete = document.querySelector(".btn-complete");
-const allMemo = [];
+
+let allMemo = JSON.parse(localStorage.getItem("allMemo"));
 
 btnComplete.addEventListener("click", addMemo);
-// title에 enter event 어떻게 달지? 왜 안 되지..
-// title.addEventListener("keyup", function (event) {
-//   if (event.keyCode == 13) {
-//     addMemo();
-//     console.log('출력');
-//   }
-// });
-
-contents.addEventListener("keyup", function (event) {
-  if (event.keyCode === 13) {
-    addMemo();
-  }
-});
-
-function addMemo() {
-  let memoObject = {
-    title: title.value,
-    contents: contents.value,
-  };
-
-  allMemo.push(memoObject);
-
-  console.log(allMemo);
-
-  title.value = "";
-  contents.value = "";
-
-  render();
-}
 
 function render() {
   const display = document.querySelector("#display");
@@ -43,24 +15,93 @@ function render() {
     const memoBox = document.createElement("div");
     const saveTitle = document.createElement("h2");
     const saveContents = document.createElement("p");
-    const btnEdit = document.createElement("button");
-    const btnDelete = document.createElement("button");
+    const btnEditEl = document.createElement("button");
+    const btnDeleteEl = document.createElement("button");
 
     memoBox.classList.add("memo-list");
-    saveTitle.classList.add("ellipsis");
-    btnEdit.setAttribute("type", "button");
-    btnDelete.setAttribute("type", "button");
-    saveContents.classList.add("multi-ellipsis");
 
+    saveTitle.classList.add("ellipsis");
     saveTitle.textContent = item.title;
+
+    saveContents.classList.add("multi-ellipsis");
     saveContents.textContent = item.contents;
-    btnEdit.textContent = "수정";
-    btnDelete.textContent = "삭제";
+
+    btnEditEl.setAttribute("type", "button");
+    btnEditEl.textContent = "수정";
+    btnEditEl.setAttribute(
+      "onclick",
+      `edit('${item.id}', '${item.title}', '${item.contents}')`
+    );
+
+    btnDeleteEl.setAttribute("type", "button");
+    btnDeleteEl.setAttribute("onclick", `remove('${item.id}')`);
+    btnDeleteEl.textContent = "삭제";
 
     display.appendChild(memoBox);
     memoBox.appendChild(saveTitle);
     memoBox.appendChild(saveContents);
-    memoBox.appendChild(btnDelete);
-    memoBox.appendChild(btnEdit);
+    memoBox.appendChild(btnDeleteEl);
+    memoBox.appendChild(btnEditEl);
   }
+}
+
+render();
+
+let editId;
+let isEditedMemo = false;
+function edit(memoId, editTitle, editContent) {
+  editId = memoId;
+  isEditedMemo = true;
+  btnComplete.textContent = "수정";
+  title.value = editTitle;
+  contents.value = editContent;
+  title.focus();
+}
+
+function addMemo() {
+  let userTitle = title.value.trim();
+  let userContent = contents.value;
+  let memoInfo;
+
+  if (userTitle) {
+    if (!isEditedMemo) {
+      if (!allMemo) {
+        allMemo = [];
+      }
+      memoInfo = {
+        id: randomID(),
+        title: title.value,
+        contents: contents.value,
+      };
+      allMemo.push(memoInfo);
+    } else {
+      isEditedMemo = false;
+      for (let i = 0; i < allMemo.length; i++) {
+        if (allMemo[i].id === editId) {
+          allMemo[i].title = userTitle;
+          allMemo[i].contents = userContent;
+        }
+      }
+      btnComplete.textContent = "완료";
+    }
+    title.value = "";
+    contents.value = "";
+  } else {
+    title.focus();
+
+  }
+
+  localStorage.setItem("allMemo", JSON.stringify(allMemo));
+  render();
+}
+
+function remove(id) {
+  allMemo.splice(id, 1);
+  isEditedMemo = false;
+  localStorage.setItem("allMemo", JSON.stringify(allMemo));
+  render();
+}
+
+function randomID() {
+  return "_" + Math.random().toString(36).substring(2, 9);
 }
