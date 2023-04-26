fffe5359c80682bfcec7eb8b4ec0229de8422778
Yuli Kim <kkimyulee@naver.com>
2023-04-26 09:40:56 +0900

    [실습] render 함수 수정

--- JavaScript/notepad/main.js
+++ JavaScript/notepad/main.js
@@ -1,4 +1,5 @@
-// 1. 사용자가 값을 입력하고, 완료 버튼을 누르면 메모가 추가된다.
+"use strict";
+
 const title = document.querySelector("#memo-title");
 const contents = document.querySelector(".memo-contents");
 const btnComplete = document.querySelector(".btn-complete");
@@ -11,37 +12,39 @@ function render() {
   const display = document.querySelector("#display");
   display.innerHTML = "";
 
-  for (const item of allMemo) {
-    const memoBox = document.createElement("div");
-    const saveTitle = document.createElement("h2");
-    const saveContents = document.createElement("p");
-    const btnEditEl = document.createElement("button");
-    const btnDeleteEl = document.createElement("button");
+  if (allMemo) {
+    for (const item of allMemo) {
+      const memoBox = document.createElement("div");
+      const saveTitle = document.createElement("h2");
+      const saveContents = document.createElement("p");
+      const btnEditEl = document.createElement("button");
+      const btnDeleteEl = document.createElement("button");
 
-    memoBox.classList.add("memo-list");
+      memoBox.classList.add("memo-list");
 
-    saveTitle.classList.add("ellipsis");
-    saveTitle.textContent = item.title;
+      saveTitle.classList.add("ellipsis");
+      saveTitle.textContent = item.title;
 
-    saveContents.classList.add("multi-ellipsis");
-    saveContents.textContent = item.contents;
+      saveContents.classList.add("multi-ellipsis");
+      saveContents.textContent = item.contents;
 
-    btnEditEl.setAttribute("type", "button");
-    btnEditEl.textContent = "수정";
-    btnEditEl.setAttribute(
-      "onclick",
-      `edit('${item.id}', '${item.title}', '${item.contents}')`
-    );
+      btnEditEl.setAttribute("type", "button");
+      btnEditEl.textContent = "수정";
+      btnEditEl.setAttribute(
+        "onclick",
+        `edit('${item.id}', '${item.title}', '${item.contents}')`
+      );
 
-    btnDeleteEl.setAttribute("type", "button");
-    btnDeleteEl.setAttribute("onclick", `remove('${item.id}')`);
-    btnDeleteEl.textContent = "삭제";
+      btnDeleteEl.setAttribute("type", "button");
+      btnDeleteEl.setAttribute("onclick", `remove('${item.id}')`);
+      btnDeleteEl.textContent = "삭제";
 
-    display.appendChild(memoBox);
-    memoBox.appendChild(saveTitle);
-    memoBox.appendChild(saveContents);
-    memoBox.appendChild(btnDeleteEl);
-    memoBox.appendChild(btnEditEl);
+      display.appendChild(memoBox);
+      memoBox.appendChild(saveTitle);
+      memoBox.appendChild(saveContents);
+      memoBox.appendChild(btnDeleteEl);
+      memoBox.appendChild(btnEditEl);
+    }
   }
 }
 
@@ -88,7 +91,6 @@ function addMemo() {
     contents.value = "";
   } else {
     title.focus();
-
   }
 
   localStorage.setItem("allMemo", JSON.stringify(allMemo));
@@ -96,7 +98,11 @@ function addMemo() {
 }
 
 function remove(id) {
-  allMemo.splice(id, 1);
+  for (let i = 0; i < allMemo.length; i++) {
+    if (allMemo[i].id === id) {
+      allMemo.splice(i, 1);
+    }
+  }
   isEditedMemo = false;
   localStorage.setItem("allMemo", JSON.stringify(allMemo));
   render();
